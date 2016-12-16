787dd8050753a23caf8ca4cc82250b3ec5bb9be0
GeorgiHr <georgiphristov96@abv.bg>
2016-12-17 00:50:47 +0200

    Adding full name property

--- App_Start/IdentityConfig.cs
+++ App_Start/IdentityConfig.cs
@@ -42,7 +42,7 @@ namespace SoftuniFinalsBlog
 
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
         {
-            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<BlogDbContext>()));
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
@@ -54,10 +54,10 @@ namespace SoftuniFinalsBlog
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
-                RequireUppercase = true,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
