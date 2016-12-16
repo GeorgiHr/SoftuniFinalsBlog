c77c54d5735ec734beb1e482847bf428ff1e8173
GeorgiHr <georgiphristov96@abv.bg>
2016-12-17 00:51:20 +0200

    Viewmodel

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -70,6 +70,11 @@ namespace SoftuniFinalsBlog.Models
         public string Email { get; set; }
 
         [Required]
+        [StringLength(50)]
+        [Display(Name = "Full name")]
+        public string FullName { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
