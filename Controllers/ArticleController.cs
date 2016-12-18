af23fa3810f33ac593a8eabd837b9637124d5cc0
GeorgiHr <georgiphristov96@abv.bg>
2016-12-18 13:22:15 +0200

    ...

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -60,13 +60,21 @@ namespace SoftuniFinalsBlog.Controllers
         [Authorize]
         public ActionResult Create()
         {
-            return View();
+            using (var database = new BlogDbContext())
+            {
+                var model = new ArticleViewModel();
+                model.Categories = database.Categories
+                    .OrderBy(c => c.Name)
+                    .ToList();
+
+                return View(model);
+            }
         }
         //
         //POST: Article/Create
         [HttpPost]
         [Authorize]
-        public ActionResult Create (Article article)
+        public ActionResult Create (ArticleViewModel model)
         {
             if (ModelState.IsValid)
             {
@@ -77,7 +85,7 @@ namespace SoftuniFinalsBlog.Controllers
                         .First()
                         .Id;
 
-                    article.AuthorId = authorId;
+                    var article = new Article(authorId, model.Title, model.Content, model.CategoryId);
 
                     database.Articles.Add(article);
                     database.SaveChanges();
@@ -85,7 +93,7 @@ namespace SoftuniFinalsBlog.Controllers
                     return RedirectToAction("Index");
                 }
             }
-            return View(article);
+            return View(model);
         }
         //
         //GET: Article/Delete
--- Models/Article.cs
+++ Models/Article.cs
@@ -9,6 +9,24 @@ namespace SoftuniFinalsBlog.Models
 {
     public class Article
     {
+        public Article()
+        {
+
+        }
+
+        public Article(string authorId, string title, string content, int categoryId)
+        {
+            this.AuthorId = authorId;
+            this.Title = title;
+            this.Content = content;
+            this.CategoryId = categoryId;
+        }
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
         [Key]
         public int Id { get; set; }
 
